8ef4c44ae85d303753b13071f04a15369ff3e76c
Jake Foraker <jake.foraker@byjakt.com>
2014-10-30 00:36:15 -0400

    api configs

--- build/src/config/api.js
+++ build/src/config/api.js
@@ -3,7 +3,7 @@ angular.module('ai.settings.api', [])
 	.constant('api', {
 		prod: 'https://jakt-lunch-api.herokuapp.com/api',
 		dev: 'http://0.0.0.0:3000/api',
-		endpoint: 'http://0.0.0.0:3000/api'
+		endpoint: 'https://jakt-lunch-api.herokuapp.com/api'
 	})
 
 ;
